3757480199c643d25e747d52d6af8ef12316857e
parkernilson <nilsopar@gmail.com>
2020-04-30 12:33:22 -0600

    write looks good test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,10 +6,11 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	vscode.window.showInformationMessage('');
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	test('Does it look good?', async () => {
+		const goodOption = "Does it look GOOD?";
+		const badOption = "Does it look BAD?";
+		assert(await vscode.window.showQuickPick([goodOption, badOption]), goodOption);
 	});
 });
