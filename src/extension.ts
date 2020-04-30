3abc18b0af02c23841b48061edbf132f87ac1531
parkernilson <nilsopar@gmail.com>
2020-04-29 21:23:19 -0600

    WIP: Try adding an activation command to get ext to work
    Extension works in debug mode, and during tests, but does not work when
    I install it as an extension in my editor. I think it might be because
    the extension is not getting activated, so I am adding an activation
    command.

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,10 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Extension "gray-php-in-html-comments" is now active!');
+
+	vscode.commands.registerCommand('grayPhpInHtmlComments.activate', () => {
+		vscode.window.showInformationMessage('Gray PHP in HTML Comments activated!');
+	});
 }
 
 // this method is called when the extension is deactivated
